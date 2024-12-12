9a8253cd6286fab4d9e9fc60ebfe3d9a6c453940
Mariella-Arias <ariasmariella.brm@gmail.com>
2024-12-12 11:08:24 -0600

    Create empty state expense list

--- index.js
+++ index.js
@@ -110,8 +110,9 @@ class App {
       day: "numeric",
     }).format(new Date());
 
-    localStorage.setItem(key, JSON.stringify(this.sampleExpenses));
+    // localStorage.setItem(key, JSON.stringify(this.sampleExpenses));
 
+    // Set today's expenses
     this.#setExpenses(key);
   }
 
@@ -131,37 +132,46 @@ class App {
     let list = document.getElementById("expense-list");
 
     let total = 0;
-    expenses.forEach(({ amount, category }, idx) => {
-      total += amount;
+    if (expenses.length) {
+      expenses.forEach(({ amount, category }, idx) => {
+        total += amount;
 
+        list.insertAdjacentHTML(
+          "afterbegin",
+          `<li>
+           <div class="expense-item">
+             <p class="body-text">${category}</p>
+             <div class="expense-amount">
+               <span>$${amount.toFixed(2)}</span>
+               <button class="btn btn-edit">
+                 <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                   </svg>
+               </button>
+             </div>
+          </div>
+          <hr />
+          </li>`
+        );
+      });
+    } else {
       list.insertAdjacentHTML(
         "afterbegin",
-        `<li>
-         <div class="expense-item">
-           <p class="body-text">${category}</p>
-           <div class="expense-amount">
-             <span>$${amount.toFixed(2)}</span>
-             <button class="btn btn-edit">
-               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                  />
-                 </svg>
-             </button>
-           </div>
-        </div>
-        <hr />
-        </li>`
+        ` <div class="empty-list">
+          <span>No expenses on this date</span>
+        </div>`
       );
-    });
+    }
 
     this.total.textContent = `$${total.toFixed(2)}`;
   }
