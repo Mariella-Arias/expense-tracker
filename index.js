3c357fe7207f388073f552b6f1d58efdc32ca4ef
Mariella-Arias <ariasmariella.brm@gmail.com>
2024-12-21 14:28:28 -0600

    Method reorg and rename

--- index.js
+++ index.js
@@ -33,13 +33,260 @@ class App {
       "change",
       this.#handleDateChange.bind(this)
     );
-    this.prevDate.addEventListener("click", this.#goBackOne.bind(this));
-    this.nextDay.addEventListener("click", this.#goForwardOne.bind(this));
+    this.prevDate.addEventListener("click", this.#goToPreviousDay.bind(this));
+    this.nextDay.addEventListener("click", this.#goToNextDay.bind(this));
     this.filterButton.addEventListener(
       "click",
       this.#toggleFilterDropdown.bind(this)
     );
     this.#init();
+  }
+
+  #init() {
+    // Get today's date
+    const locale = navigator.language;
+    const year = new Intl.DateTimeFormat(locale, {
+      year: "numeric",
+    }).format(new Date());
+    const month = new Intl.DateTimeFormat(locale, {
+      month: "numeric",
+    }).format(new Date());
+    const day = new Intl.DateTimeFormat(locale, {
+      day: "numeric",
+    }).format(new Date());
+
+    this.currentDate.setAttribute("value", `${year}-${month}-${day}`);
+    this.currentDate.setAttribute("max", `${year}-${month}-${day}`);
+
+    const key = new Intl.DateTimeFormat(locale, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    }).format(new Date());
+
+    // Set expenses
+    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
+
+    this.#refreshContent();
+  }
+
+  #refreshContent() {
+    const list = document.getElementById("expense-list");
+
+    let total = 0;
+
+    const activeFilters = Object.values(this.activeFilters).reduce(
+      (acc, curr) => acc + (curr ? 1 : 0),
+      0
+    );
+
+    // Clear any elements that may already be in the list
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+
+    if (this.expenses.length) {
+      this.expenses.forEach(({ amount, category }, idx) => {
+        if (activeFilters) {
+          if (this.activeFilters[category]) {
+            total += Number(amount);
+
+            list.insertAdjacentHTML(
+              "afterbegin",
+              `<li id="expense-${idx}">
+                 <div class="expense-item">
+                   <p class="body-text">${
+                     category.charAt(0).toUpperCase() + category.slice(1)
+                   }
+                   </p>
+                   <div class="expense-amount">
+                     <span>$${Number(amount).toFixed(2)}</span>
+                     <button id="expense-options-${idx}" class="btn btn-open-tooltip">
+                     <svg
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     stroke-width="1.5"
+                     stroke="currentColor"
+                     >
+                     <path
+                     stroke-linecap="round"
+                     stroke-linejoin="round"
+                     d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                     />
+                     </svg>
+                     </button>
+                   </div>
+                </div>
+                <hr />
+                </li>`
+            );
+
+            const btnOpenTooltip = document.querySelector(".btn-open-tooltip");
+            btnOpenTooltip.addEventListener("click", (e) => {
+              this.#openTooltip.call(this, e, idx);
+            });
+          }
+        } else {
+          total += Number(amount);
+
+          list.insertAdjacentHTML(
+            "afterbegin",
+            `<li id="expense-${idx}">
+               <div class="expense-item">
+                 <p class="body-text">${
+                   category.charAt(0).toUpperCase() + category.slice(1)
+                 }
+                 </p>
+                 <div class="expense-amount">
+                   <span>$${Number(amount).toFixed(2)}</span>
+                   <button id="expense-options-${idx}" class="btn btn-open-tooltip">
+                   <svg
+                   xmlns="http://www.w3.org/2000/svg"
+                   fill="none"
+                   viewBox="0 0 24 24"
+                   stroke-width="1.5"
+                   stroke="currentColor"
+                   >
+                   <path
+                   stroke-linecap="round"
+                   stroke-linejoin="round"
+                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                   />
+                   </svg>
+                   </button>
+                 </div>
+              </div>
+              <hr />
+              </li>`
+          );
+
+          const btnOpenTooltip = document.querySelector(".btn-open-tooltip");
+          btnOpenTooltip.addEventListener("click", (e) => {
+            this.#openTooltip.call(this, e, idx);
+          });
+        }
+      });
+    } else {
+      list.insertAdjacentHTML(
+        "afterbegin",
+        ` <div class="empty-list">
+            <span>No expenses on this date</span>
+          </div>`
+      );
+    }
+
+    this.total.textContent = `$${Number(total).toFixed(2)}`;
+  }
+
+  #handleDateChange(e) {
+    const locale = navigator.language;
+
+    let key = new Intl.DateTimeFormat(locale, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      timeZone: "UTC",
+    }).format(new Date(e.target.value));
+
+    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
+
+    this.#refreshContent();
+  }
+
+  #goToPreviousDay() {
+    let [year, monthIndex, day] = this.currentDate.value.split("-");
+
+    let yesterday = new Date(Number(year), Number(monthIndex) - 1, Number(day));
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const locale = navigator.language;
+
+    const prevYear = new Intl.DateTimeFormat(locale, {
+      year: "numeric",
+    }).format(new Date(yesterday));
+    let prevMonth = new Intl.DateTimeFormat(locale, {
+      month: "numeric",
+    }).format(new Date(yesterday));
+    let prevDay = new Intl.DateTimeFormat(locale, {
+      day: "numeric",
+    }).format(new Date(yesterday));
+
+    if (prevMonth < 10) {
+      prevMonth = "0" + prevMonth;
+    }
+
+    if (prevDay < 10) {
+      prevDay = "0" + prevDay;
+    }
+
+    this.currentDate.value = `${prevYear}-${prevMonth}-${prevDay}`;
+
+    const key = new Intl.DateTimeFormat(locale, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      timeZone: "UTC",
+    }).format(new Date(yesterday));
+
+    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
+
+    this.#refreshContent();
+  }
+
+  #goToNextDay() {
+    const [year, monthIndex, day] = this.currentDate.value.split("-");
+
+    const tomorrow = new Date(
+      Number(year),
+      Number(monthIndex) - 1,
+      Number(day)
+    );
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    const [maxYear, maxMonthIndex, maxDay] = this.currentDate.max.split("-");
+
+    const max = new Date(
+      Number(maxYear),
+      Number(maxMonthIndex) - 1,
+      Number(maxDay)
+    );
+
+    if (tomorrow > max) {
+      return;
+    }
+
+    const locale = navigator.language;
+    const nextYear = new Intl.DateTimeFormat(locale, {
+      year: "numeric",
+    }).format(new Date(tomorrow));
+    let nextMonth = new Intl.DateTimeFormat(locale, {
+      month: "numeric",
+    }).format(new Date(tomorrow));
+    let nextDay = new Intl.DateTimeFormat(locale, {
+      day: "numeric",
+    }).format(new Date(tomorrow));
+
+    if (nextMonth < 10) {
+      nextMonth = "0" + nextMonth;
+    }
+
+    if (nextDay < 10) {
+      nextDay = "0" + nextDay;
+    }
+
+    this.currentDate.value = `${nextYear}-${nextMonth}-${nextDay}`;
+
+    const key = new Intl.DateTimeFormat(locale, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      timeZone: "UTC",
+    }).format(new Date(tomorrow));
+
+    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
+
+    this.#refreshContent();
   }
 
   #toggleFilterDropdown(e) {
@@ -62,8 +309,18 @@ class App {
         )
         .join("");
 
-      const dropdown = `<form id="filters" class="filter-options">${filterOptions}<button id="clear-filters" type="button" class="btn btn-clear"><span>Clear</span></button> 
-          </form>`;
+      const dropdown = `
+      <form 
+       id="filters" 
+       class="filter-options">
+        ${filterOptions}
+        <button 
+         id="clear-filters" 
+         type="button" 
+         class="btn btn-clear">
+        <span>Clear</span>
+        </button> 
+      </form>`;
 
       this.filterButton.insertAdjacentHTML("afterend", dropdown);
 
@@ -195,158 +452,6 @@ class App {
     this.newExpenseForm.reset();
     this.modal.removeEventListener("click", this.#handleClickOutsideModal);
     this.modal.close();
-  }
-
-  #init() {
-    // Get today's date
-    const locale = navigator.language;
-    const year = new Intl.DateTimeFormat(locale, {
-      year: "numeric",
-    }).format(new Date());
-    const month = new Intl.DateTimeFormat(locale, {
-      month: "numeric",
-    }).format(new Date());
-    const day = new Intl.DateTimeFormat(locale, {
-      day: "numeric",
-    }).format(new Date());
-
-    this.currentDate.setAttribute("value", `${year}-${month}-${day}`);
-    this.currentDate.setAttribute("max", `${year}-${month}-${day}`);
-
-    const key = new Intl.DateTimeFormat(locale, {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-    }).format(new Date());
-
-    // Set expenses
-    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
-
-    this.#refreshContent();
-  }
-
-  #handleDateChange(e) {
-    const locale = navigator.language;
-
-    let key = new Intl.DateTimeFormat(locale, {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-      timeZone: "UTC",
-    }).format(new Date(e.target.value));
-
-    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
-
-    this.#refreshContent();
-  }
-
-  #refreshContent() {
-    const list = document.getElementById("expense-list");
-
-    let total = 0;
-
-    const activeFilters = Object.values(this.activeFilters).reduce(
-      (acc, curr) => acc + (curr ? 1 : 0),
-      0
-    );
-
-    // Clear any elements that may already be in the list
-    while (list.firstChild) {
-      list.removeChild(list.firstChild);
-    }
-
-    if (this.expenses.length) {
-      this.expenses.forEach(({ amount, category }, idx) => {
-        if (activeFilters) {
-          if (this.activeFilters[category]) {
-            total += Number(amount);
-
-            list.insertAdjacentHTML(
-              "afterbegin",
-              `<li id="expense-${idx}">
-                 <div class="expense-item">
-                   <p class="body-text">${
-                     category.charAt(0).toUpperCase() + category.slice(1)
-                   }
-                   </p>
-                   <div class="expense-amount">
-                     <span>$${Number(amount).toFixed(2)}</span>
-                     <button id="expense-options-${idx}" class="btn btn-open-tooltip">
-                     <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 24 24"
-                     stroke-width="1.5"
-                     stroke="currentColor"
-                     >
-                     <path
-                     stroke-linecap="round"
-                     stroke-linejoin="round"
-                     d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                     />
-                     </svg>
-                     </button>
-                   </div>
-                </div>
-                <hr />
-                </li>`
-            );
-
-            const btnOpenTooltip = document.querySelector(".btn-open-tooltip");
-            btnOpenTooltip.addEventListener("click", (e) => {
-              this.#openTooltip.call(this, e, idx);
-            });
-          }
-        } else {
-          total += Number(amount);
-
-          list.insertAdjacentHTML(
-            "afterbegin",
-            `<li id="expense-${idx}">
-               <div class="expense-item">
-                 <p class="body-text">${
-                   category.charAt(0).toUpperCase() + category.slice(1)
-                 }
-                 </p>
-                 <div class="expense-amount">
-                   <span>$${Number(amount).toFixed(2)}</span>
-                   <button id="expense-options-${idx}" class="btn btn-open-tooltip">
-                   <svg
-                   xmlns="http://www.w3.org/2000/svg"
-                   fill="none"
-                   viewBox="0 0 24 24"
-                   stroke-width="1.5"
-                   stroke="currentColor"
-                   >
-                   <path
-                   stroke-linecap="round"
-                   stroke-linejoin="round"
-                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                   />
-                   </svg>
-                   </button>
-                 </div>
-              </div>
-              <hr />
-              </li>`
-          );
-
-          const btnOpenTooltip = document.querySelector(".btn-open-tooltip");
-          btnOpenTooltip.addEventListener("click", (e) => {
-            this.#openTooltip.call(this, e, idx);
-          });
-        }
-      });
-    } else {
-      list.insertAdjacentHTML(
-        "afterbegin",
-        ` <div class="empty-list">
-            <span>No expenses on this date</span>
-          </div>`
-      );
-    }
-
-    this.total.textContent = `$${Number(total).toFixed(2)}`;
   }
 
   #openTooltip(e, id) {
@@ -501,101 +606,6 @@ class App {
     }).format(new Date(this.currentDate.value));
 
     localStorage.setItem(key, JSON.stringify(this.expenses));
-
-    this.#refreshContent();
-  }
-
-  #goBackOne() {
-    let [year, monthIndex, day] = this.currentDate.value.split("-");
-
-    let yesterday = new Date(Number(year), Number(monthIndex) - 1, Number(day));
-    yesterday.setDate(yesterday.getDate() - 1);
-
-    const locale = navigator.language;
-
-    const prevYear = new Intl.DateTimeFormat(locale, {
-      year: "numeric",
-    }).format(new Date(yesterday));
-    let prevMonth = new Intl.DateTimeFormat(locale, {
-      month: "numeric",
-    }).format(new Date(yesterday));
-    let prevDay = new Intl.DateTimeFormat(locale, {
-      day: "numeric",
-    }).format(new Date(yesterday));
-
-    if (prevMonth < 10) {
-      prevMonth = "0" + prevMonth;
-    }
-
-    if (prevDay < 10) {
-      prevDay = "0" + prevDay;
-    }
-
-    this.currentDate.value = `${prevYear}-${prevMonth}-${prevDay}`;
-
-    const key = new Intl.DateTimeFormat(locale, {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-      timeZone: "UTC",
-    }).format(new Date(yesterday));
-
-    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
-
-    this.#refreshContent();
-  }
-
-  #goForwardOne() {
-    const [year, monthIndex, day] = this.currentDate.value.split("-");
-
-    const tomorrow = new Date(
-      Number(year),
-      Number(monthIndex) - 1,
-      Number(day)
-    );
-    tomorrow.setDate(tomorrow.getDate() + 1);
-
-    const [maxYear, maxMonthIndex, maxDay] = this.currentDate.max.split("-");
-
-    const max = new Date(
-      Number(maxYear),
-      Number(maxMonthIndex) - 1,
-      Number(maxDay)
-    );
-
-    if (tomorrow > max) {
-      return;
-    }
-
-    const locale = navigator.language;
-    const nextYear = new Intl.DateTimeFormat(locale, {
-      year: "numeric",
-    }).format(new Date(tomorrow));
-    let nextMonth = new Intl.DateTimeFormat(locale, {
-      month: "numeric",
-    }).format(new Date(tomorrow));
-    let nextDay = new Intl.DateTimeFormat(locale, {
-      day: "numeric",
-    }).format(new Date(tomorrow));
-
-    if (nextMonth < 10) {
-      nextMonth = "0" + nextMonth;
-    }
-
-    if (nextDay < 10) {
-      nextDay = "0" + nextDay;
-    }
-
-    this.currentDate.value = `${nextYear}-${nextMonth}-${nextDay}`;
-
-    const key = new Intl.DateTimeFormat(locale, {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-      timeZone: "UTC",
-    }).format(new Date(tomorrow));
-
-    this.expenses = JSON.parse(localStorage.getItem(key)) || [];
 
     this.#refreshContent();
   }
