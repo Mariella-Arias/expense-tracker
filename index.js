43c1f516befc3983385276b281efb83db011a18a
Mariella-Arias <ariasmariella.brm@gmail.com>
2024-12-31 11:11:17 -0600

    Update syntax to use enableBindings boolean flag

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import ExpensesController from "./js/controllers/ExpensesController.js";
 document.addEventListener("DOMContentLoaded", () => {
   const model = new ExpensesModel();
   const view = new ExpensesView();
-  new ExpensesController(model, view);
+  new ExpensesController(model, view, { enableBindings: true });
 });
